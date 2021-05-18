541b6748adca4f1e4b64c05a64c595333e13af8b
AmilMasic <amil.masic@gmail.com>
2021-05-18 17:45:56 -0600

    added handleOnChange function and form atributes name value and onchange in publisherInput

--- src/components/publisherInput.js
+++ src/components/publisherInput.js
@@ -8,6 +8,12 @@ export default class PublisherInput extends React.Component {
     gamingStore: ''
   }
 
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render(){
     return(
       <div>
@@ -17,18 +23,24 @@ export default class PublisherInput extends React.Component {
             type='text'
             placeholder='name'
             value={this.state.name}
+            name='name'
+            onChange={this.handleOnChange}
           /><br/>
           <label>Established: </label>
           <input
             type='text'
             placeholder='1986'
             value={this.state.established}
+            name='established'
+            onChange={this.handleOnChange}
           /><br/>
           <label>Gaming Store: </label>
           <input
             type='text'
             placeholder='Origin'
             value={this.state.gamingStore}
+            name='gamingStore'
+            onChange={this.handleOnChange}
           /><br/>
         </form>
       </div>
