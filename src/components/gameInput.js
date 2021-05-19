6b2932a3f6e673a845ced64cc66e0da12c43558b
AmilMasic <amil.masic@gmail.com>
2021-05-19 09:57:18 -0600

    init boilerplate for gameinput component

---
+++ src/components/gameInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class GameInput extends React.Component {
+
+  render(){
+    return (
+      <div>
+        GameInput
+      </div>
+    )
+  }
+
+}
+
+export default GameInput
