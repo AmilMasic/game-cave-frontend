21e5fbfe51ff7b87e01be27f9f01ce0faff2a460
AmilMasic <amil.masic@gmail.com>
2021-05-19 09:29:33 -0600

    updated publishers component to link to publishers

--- src/components/publishers.js
+++ src/components/publishers.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
 import Publisher from './publisher';
 
 const Publishers = (props) => {
   return (
     <div>
-      {props.publishers.map(publisher => <div key={publisher.id}><Publisher publisher={publisher} /></ div>)}
+      {props.publishers.map(publisher =>
+        <div key={publisher.id}>
+          < Link to={`/publishers/${publisher.id}`}>{publisher.name}</Link>
+        </ div>)}
     </div>
   )
 }
