ceec047f5d2847fac9175f78d09b47b57aed9690
AmilMasic <amil.masic@gmail.com>
2021-05-19 10:02:35 -0600

    init boilerplate for games component and connected gameinput and games to games container

--- src/containers/gamesContainer.js
+++ src/containers/gamesContainer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import GameInput from '../components/gameInput';
+import Games from '../components/games';
+
 class GamesContainer extends React.Component {
 
   render(){
     return (
       <div>
         GamesContainer
+        <GameInput />
+        <Games />
       </div>
     )
   }
