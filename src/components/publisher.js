f0169b2aaacb592d5924bdf3174aa2b56780868d
AmilMasic <amil.masic@gmail.com>
2021-05-19 07:57:16 -0600

    successfully seperated publisher into its own component that connects properly to publishers component

--- src/components/publisher.js
+++ src/components/publisher.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Publisher = (props) => {
 
-  return {
-    <div>
-
-    </div>
-  }
+  return (
+    <li>
+      {props.publisher.name} - {props.publisher.established} - {props.publisher.gamestore}
+    </li>
+  )
 }
 
 export default Publisher;
--- src/components/publishers.js
+++ src/components/publishers.js
@@ -4,7 +4,7 @@ import Publisher from './publisher';
 const Publishers = (props) => {
   return (
     <div>
-      {props.publishers.map(publisher => <li key={publisher.id}><Publisher publisher={publisher} /></ li>)}
+      {props.publishers.map(publisher => <div key={publisher.id}><Publisher publisher={publisher} /></ div>)}
     </div>
   )
 }
