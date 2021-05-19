d48ea8091ad86f8b093d6338f4e77cc799178f7a
AmilMasic <amil.masic@gmail.com>
2021-05-19 07:49:27 -0600

    changed publishers.js to include publisher.js

--- src/components/publishers.js
+++ src/components/publishers.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Publisher from './publisher';
 
 const Publishers = (props) => {
   return (
     <div>
-      {props.publishers.map(publisher => <li key={publisher.id}>{publisher.name} - Established: {publisher.established} - gamestore: {publisher.gamestore}</ li>)}
+      {props.publishers.map(publisher => <li key={publisher.id}><Publisher publisher={publisher} /></ li>)}
     </div>
   )
 }
