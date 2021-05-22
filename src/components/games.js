b5eb0dab3766be9a59468bf4b7d0ac81878c19a2
AmilMasic <amil.masic@gmail.com>
2021-05-22 12:45:13 -0700

    added delete functionality to games component

--- src/components/games.js
+++ src/components/games.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {deleteGame} from '../actions/deleteGame';
 
 
 const Games = (props) => {
+
+  const handleDelete = (game) => {
+  
+    props.deleteGame(game.id, game.publisher_id)
+  }
   return (
     <div>
       {props.games && props.games.map(game =>
-        <li key={game.id}>{game.title} - {game.genre}</li>
+        <li key={game.id}>{game.title} - {game.genre}<button onClick={() => handleDelete(game)}>Delete</button></li>
       )}
     </div>
   )
 }
 
-export default Games;
+export default connect(null, {deleteGame})(Games);
