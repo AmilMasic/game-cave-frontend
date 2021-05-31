2fcea60d3e5b41081b39ef156ff23a485ce530e8
AmilMasic <amil.masic@gmail.com>
2021-05-31 13:53:40 -0600

    restored delete capabalities of games component

--- src/components/games.js
+++ src/components/games.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Route, Link} from 'react-router-dom';
-// import {connect} from 'react-redux';
-// import {deleteGame} from '../actions/deleteGame';
+import {connect} from 'react-redux';
+import {deleteGame} from '../actions/deleteGame';
 import Game from './game';
 
 
 const Games = (props) => {
 
+  const handleDelete = (game) => {
 
+    props.deleteGame(game.id, game.publisher_id)
+  }
 
   return (
 
@@ -16,9 +19,10 @@ const Games = (props) => {
      {props.games && props.games.map(game =>
       <div key={game.id}>
         < Link to={`/publishers/${game.publisher_id}/games/${game.id}`}>{game.title}</Link>
+        <button id='delete' onClick={() => handleDelete(game)}>Delete</button>
       </ div>)}
     </div>
   )
 }
 
-export default Games;
+export default connect(null, {deleteGame})(Games);
