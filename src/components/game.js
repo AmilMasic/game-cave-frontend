fd51d18b8158308ebf92ce4fc0801ad6f7ad9eb2
AmilMasic <amil.masic@gmail.com>
2021-05-31 13:35:06 -0600

    updated game component

--- src/components/game.js
+++ src/components/game.js
@@ -4,14 +4,28 @@ import React from 'react';
 
 const Game = (props) => {
 
-  // let publisher = props.publishers[props.match.params.id - 1];
-  
-  // let game = props.games.filter(game => game.id == props.match.params.id)[0]
+  const handleDelete = (game) => {
 
+    props.deleteGame(game.id, game.publisher_id)
+  }
+
+
+  let publisher = props.publishers.find(publisher => publisher.id == props.match.params.id)
+  let game = publisher.games.find(game => game.id == props.match.params.game_id)
+
+  // debugger;
   return (
     <div>
-    Game Component
-
+    <h2>
+      Game Title: {game ? game.title : null}
+    </h2>
+    <h4>
+      Game Genre: {game ? game.genre : null}
+    </h4>
+    <p>
+      Description {game ? game.description : null}
+    </p>
+  
     </div>
   )
 }
